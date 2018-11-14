0cf51fccbfbe8a2bfb80a4a347c319f7a6c942bc
Christina <christina.hsu.ca@gmail.com>
2018-11-14 02:33:59 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,39 @@
+var raining = true;
+var cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+var temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+//We read the expression in the above example usually as "if not raining", but it's not always intuitive what "not true" or "not false" mean unless you remember that the ! operator simply reverses the value of raining. That is, if raining is true, !raining is false. If raining is false, !raining is true.
